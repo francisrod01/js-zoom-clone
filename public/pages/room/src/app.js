fe552ba7d121abf3de6069f5fbcc2a10f3ba9765
Francis Rodrigues <francis.rod01@outlook.com>
2021-01-27 11:56:57 -0300

    wip(public): socket builder was implemented

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -13,14 +13,20 @@ const onload = () => {
   const room = urlParams.get('room');
   console.log('this is the room:', room)
 
-  const recorderBtn = document.getElementById('record')
-  recorderBtn.addEventListener('click', recordClick(recorderBtn))
+  const socketUrl = 'http://localhost:3000';
+  const socketBuilder = new SocketBuilder({ socketUrl });
 
   const view = new View();
   const media = new Media();
-  view.renderVideo({ userId: 'test01', url: 'https://media.giphy.com/media/VfzJD0dNOXdOX9UsV6/giphy.mp4' });
-  view.renderVideo({ userId: 'test01', isCurrentId: true, url: 'https://media.giphy.com/media/VfzJD0dNOXdOX9UsV6/giphy.mp4' });
-  view.renderVideo({ userId: 'test02', url: 'https://media.giphy.com/media/VfzJD0dNOXdOX9UsV6/giphy.mp4' });
+
+  const deps = {
+    view,
+    media,
+    room,
+    socketBuilder,
+  };
+
+  Business.initialize(deps);
 }
 
 window.onload = onload
