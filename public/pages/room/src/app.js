0553530a84ef6ffcdadc6d989fe1007de32ff3a3
Francis Rodrigues <francis.rod01@outlook.com>
2021-01-27 11:54:20 -0300

    enh: Moved pages into public folder

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -11,11 +11,16 @@ const recordClick = function (recorderBtn) {
 const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const room = urlParams.get('room');
-  console.log('this is the room', room)
+  console.log('this is the room:', room)
 
   const recorderBtn = document.getElementById('record')
   recorderBtn.addEventListener('click', recordClick(recorderBtn))
 
+  const view = new View();
+  const media = new Media();
+  view.renderVideo({ userId: 'test01', url: 'https://media.giphy.com/media/VfzJD0dNOXdOX9UsV6/giphy.mp4' });
+  view.renderVideo({ userId: 'test01', isCurrentId: true, url: 'https://media.giphy.com/media/VfzJD0dNOXdOX9UsV6/giphy.mp4' });
+  view.renderVideo({ userId: 'test02', url: 'https://media.giphy.com/media/VfzJD0dNOXdOX9UsV6/giphy.mp4' });
 }
 
 window.onload = onload
