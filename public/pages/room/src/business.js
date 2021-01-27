67e0d0e51a9250053a9487223786b90e2d588f5f
Francis Rodrigues <francis.rod01@outlook.com>
2021-01-27 12:02:51 -0300

    wip(public) A business class was implemented
    This business logic is for video and room stream management

---
+++ public/pages/room/src/business.js
@@ -0,0 +1,45 @@
+class Business {
+  constructor({ room, media, view, socketBuilder }) {
+    this.room = room;
+    this.media = media;
+    this.view = view;
+
+    this.socketBuilder = socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .build();
+    this.socketBuilder.emit('join-room', this.room, 'test1');
+
+    this.currentStream = {};
+  }
+
+  static initialize(deps) {
+    const instance = new Business(deps);
+    return instance._init();
+  }
+
+  async _init() {
+    this.currentStream = await this.media.getCamera();
+    this.addVideoStream('test01');
+  }
+
+  addVideoStream(userId, stream = this.currentStream) {
+    const isCurrentId = false;
+    this.view.renderVideo({
+      userId,
+      stream,
+      isCurrentId
+    });
+  }
+
+  onUserConnected = function() {
+    return userId => {
+      console.log('user connected!', userId);
+    };
+  }
+
+  onUserDisconnected = function() {
+    return userId => {
+      console.log('user disconnected!', userId);
+    };
+  }
+}
