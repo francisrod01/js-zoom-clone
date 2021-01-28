364a8a3c4e90fb09bf0defef4ca513a338763ae6
Francis Rodrigues <francis.rod01@outlook.com>
2021-01-28 00:33:24 -0300

    wip(public): A peer builder was set up in app

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -16,6 +16,16 @@ const onload = () => {
   const socketUrl = 'http://localhost:3000';
   const socketBuilder = new SocketBuilder({ socketUrl });
 
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/',
+    },
+  });
+  const peerBuilder = new PeerBuilder({ peerConfig });
+
   const view = new View();
   const media = new Media();
 
@@ -24,6 +34,7 @@ const onload = () => {
     media,
     room,
     socketBuilder,
+    peerBuilder,
   };
 
   Business.initialize(deps);
